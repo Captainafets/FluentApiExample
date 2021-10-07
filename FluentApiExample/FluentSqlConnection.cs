bbad2daa1dee0dc02bb6e73ee251f45afcd5bb93
Captainafets <aLinktothegithub1>
2021-10-07 23:28:03 +0200

    Initial Commit

---
+++ FluentApiExample/FluentSqlConnection.cs
@@ -0,0 +1,86 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace FluentApiExample
+{
+    public class FluentSqlConnection :
+        IServerSelectionStage,
+        IDatabaseSelectionStage,
+        IUserSelectionStage,
+        IPasswordSelectionStage,
+        IConnectionInitializerStage
+    {
+        private string _server = null!;
+        private string _database = null!;
+        private string _user = null!;
+        private string _password = null!;
+
+        private FluentSqlConnection()
+        {
+        }
+
+        public static IServerSelectionStage CreateConnection()
+        {
+            return new FluentSqlConnection();
+        }
+
+        public IDatabaseSelectionStage ForServer(string server)
+        {
+            _server = server;
+            return this;
+        }
+
+        public IUserSelectionStage AndDatabase(string database)
+        {
+            _database = database;
+            return this;
+        }
+
+        public IPasswordSelectionStage AsUser(string user)
+        {
+            _user = user;
+            return this;
+        }
+
+        public IConnectionInitializerStage WithPassword(string password)
+        {
+            _password = password;
+            return this;
+        }
+
+        public IDbConnection Connect()
+        {
+            var connection =
+                new SqlConnection($"Server={_server};Database={_database};User Id={_user};Password={_password};");
+
+            connection.Open();
+
+            return connection;
+        }
+    }
+
+    public interface IServerSelectionStage
+    {
+        public IDatabaseSelectionStage ForServer(string server);
+    }
+
+    public interface IDatabaseSelectionStage
+    {
+        public IUserSelectionStage AndDatabase(string database);
+    }
+
+    public interface IUserSelectionStage
+    {
+        public IPasswordSelectionStage AsUser(string user);
+    }
+
+    public interface IPasswordSelectionStage
+    {
+        public IConnectionInitializerStage WithPassword(string password);
+    }
+
+    public interface IConnectionInitializerStage
+    {
+        public IDbConnection Connect();
+    }
+}
---
+++ FluentApiExample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FluentApiExample
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            var connection = FluentSqlConnection.CreateConnection()
+                .ForServer("myServer")
+                .AndDatabase("myDatabase")
+                .AsUser("user")
+                .WithPassword("password")
+                .Connect();
+        }
+    }
+}
